e001ddcbb309036f459f9800ebc9c99096c954ad
ShivKJ <shivkj001@gmail.com>
2020-04-27 00:05:13 +0530

    adding resource file

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 setup(
     name='development',
     version='0.1',
-    packages=['development', 'development.math'],
+    packages=['development', 'development.math',
+              'development.using_resources'],
+    package_data={'development': ['data/info.json']},
     url='https://github.com/ShivKJ/development-practice',
     license='',
     author='Shiv',
