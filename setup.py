6a2e44cb2011d7c440be88eee42552215ffaac59
ShivKJ <shivkj001@gmail.com>
2020-04-27 01:50:18 +0530

    adding dependencies from requirement.txt in setup

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+dependencies = list(map(str.strip, open('requirement.txt').readlines()))
+
 setup(
     name='development',
     version='0.1',
@@ -7,7 +9,8 @@ setup(
               'development.using_resources'],
     package_data={'development': ['data/info.json']},
     url='https://github.com/ShivKJ/development-practice',
-    license='',
+    license='MIT License',
+    install_requires=dependencies,
     author='Shiv',
     author_email='shivkj001@gmail.com',
     description='development-practice'
